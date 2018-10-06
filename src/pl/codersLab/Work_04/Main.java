145949e3087f84ff8edede6cbb135a7e7d4f8630
Damian <Damian.nieweg@gmail.comgit config --global user.email Damian.nieweg@gmail.com>
2018-10-06 16:42:01 +0200

    Second commit

--- src/pl/codersLab/Work_04/Main.java
+++ src/pl/codersLab/Work_04/Main.java
@@ -1,7 +1,37 @@
 package pl.codersLab.Work_04;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.StringTokenizer;
+
 public class Main {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        System.out.println(throwDice("rzuc 1D20+0"));
+    }
+
+    private static Integer throwDice(String throwCommand) {
+        Random random = new Random();
+        Integer result = 0;
+        String diceDeclaration = throwCommand.split(" ")[1];
+
+        int dIndex = diceDeclaration.indexOf("D");
+        int plusIndex = diceDeclaration.indexOf("+");
+
+        String throwCountStr = diceDeclaration.substring(0, dIndex);
+        String diceTypeStr = diceDeclaration.substring(dIndex + 1, plusIndex);
+        String bonusStr = diceDeclaration.substring(plusIndex + 1);
+
+        Integer throwCount = new Integer(throwCountStr);
+        Integer diceType = new Integer(diceTypeStr);
+        Integer bonus = new Integer(bonusStr);
+
+        for(int i = 0 ; i < throwCount ; i++){
+            result = random.nextInt(diceType) + 1;
+        }
+
+        result += bonus;
+
+        return result;
     }
 }
